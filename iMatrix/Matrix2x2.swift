2b02303f999672352b5d7f1fb19e60ed2f0b4b29
Giulio Tavella <giulioTavella@MacBook-Pro-di-Giulio.local>
2017-03-30 14:10:19 +0200

    first commit

---
+++ iMatrix/Matrix2x2.swift
@@ -0,0 +1,35 @@
+//
+//  Matrix2x2.swift
+//  iMatrix
+//
+//  Created by Giulio Tavella on 02/02/17.
+//  Copyright © 2017 Giulio Tavella. All rights reserved.
+//
+
+import UIKit
+
+class Matrix2x2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iMatrix/functions.swift
@@ -0,0 +1,9 @@
+//
+//  functions.swift
+//  iMatrix
+//
+//  Created by Giulio Tavella on 05/02/17.
+//  Copyright © 2017 Giulio Tavella. All rights reserved.
+//
+
+import Foundation
